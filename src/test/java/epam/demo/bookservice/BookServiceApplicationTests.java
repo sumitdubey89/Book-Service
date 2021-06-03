0b3fc45b78f1d559105db93e3f0ca31e8c5c515e
egrc3uh <sumit.dubey@mckesson.com>
2021-06-03 13:34:57 +0530

    initial

---
+++ src/test/java/epam/demo/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.demo.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
