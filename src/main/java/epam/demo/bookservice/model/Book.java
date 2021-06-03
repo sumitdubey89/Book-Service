1ba9944682953174bd19a475c4c4028d4ea7c347
egrc3uh <sumit.dubey@mckesson.com>
2021-06-03 15:47:05 +0530

    Model and Controller class created

---
+++ src/main/java/epam/demo/bookservice/model/Book.java
@@ -0,0 +1,48 @@
+package epam.demo.bookservice.model;
+
+public class Book {
+
+	private String name;
+	private String author;
+	private String Category;
+	private String Description;
+
+	public Book(String name, String author, String category, String description) {
+		this.name = name;
+		this.author = author;
+		Category = category;
+		Description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getCategory() {
+		return Category;
+	}
+
+	public void setCategory(String category) {
+		Category = category;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+}
